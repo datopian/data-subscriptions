257b2a7f433168c30c43d012ead7577dd840e91e
Irio Musskopf <irio@protonmail.com>
2020-03-18 14:39:09 +0100

    Remove unnecessary comment

--- data_subscriptions/extensions.py
+++ data_subscriptions/extensions.py
@@ -1,8 +1,3 @@
-"""Extensions registry
-
-All extensions here are used as singletons and
-initialized in application factory
-"""
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
