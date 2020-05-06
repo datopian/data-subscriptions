07b98d47eb15f807de61c32089816076747672b8
julietezekwe <mac@MACs-MacBook-Pro.local>
2020-05-06 13:21:35 +0100

    [data subscription][s]: change the and change the download file name

--- data_subscriptions/api/resources/stat.py
+++ data_subscriptions/api/resources/stat.py
@@ -4,7 +4,6 @@ from flask import Response
 from data_subscriptions.models import Subscription as Model
 from data_subscriptions.notifications.ckan_metadata import CKANMetadata
 
-
 def create_csv(dataset, columns):
     header = ",".join(columns)
     content = header + "\n"
@@ -14,24 +13,21 @@ def create_csv(dataset, columns):
         content += row + "\n"
     return content
 
-
-def get_user(user_id):
-    result = CKANMetadata("user_show", [user_id])()
-    if user_id in result:
-        return result[user_id]
-    return {"display_name": ""}
-
-
-def get_dataset(dataset_id):
-    result = CKANMetadata("package_show", [dataset_id])()
-    if dataset_id in result:
-        return result[dataset_id]
-    return {"name": ""}
-
+def get_user(data_id, action):
+    result = CKANMetadata(action, [data_id])()
+    if data_id in result:
+        return result[data_id]
+    return {}
 
 def prepare_stat(data):
-    dataset_name = get_dataset(data.dataset_id)["name"]
-    user_name = get_user(data.user_id)["display_name"]
+    dataset_name = ""
+    user_name = ""
+    dataset = get_user(data.dataset_id, "package_show")
+    user = get_user(data.user_id, "user_show")
+    if "name" in dataset:
+        dataset_name = dataset["name"]
+    if "display_name" in user:
+        user_name = user["display_name"]
 
     return {
         "subscribed_at": str(data.created_at),
@@ -40,8 +36,6 @@ def prepare_stat(data):
         "dataset_id": data.dataset_id,
         "dataset_name": dataset_name,
     }
-
-
 class Stat(Resource):
     def get(self):
         dataset = Model.query.all()
@@ -59,6 +53,6 @@ class Stat(Resource):
             return Response(
                 csv,
                 mimetype="text/csv",
-                headers={"Content-disposition": "attachment; filename=myplot.csv"},
+                headers={"Content-disposition": "attachment; filename=data_subscribers.csv"},
             )
         return {"result": list(result)}
