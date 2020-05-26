58d20f5f07d250701a82bd69b38ec92db15aba2e
Sagar Ghimire <info@sagarg.com.np>
2020-05-26 11:46:05 +0545

    [xs]: code formatted

--- tests/test_subscriptions.py
+++ tests/test_subscriptions.py
@@ -130,8 +130,10 @@ def test_delete_subscription_by_dataset(
     mocker, ckan_meta_fixture, client, db, all_subscription, all_subscription_list
 ):
     # Return 204 NO CONTENT when subscription deleted by dataset id.
-    dispatcher =  mocker.patch("data_subscriptions.api.resources.subscribed_dataset.NotSubscribableNotifiationDispatcher")
-    
+    dispatcher = mocker.patch(
+        "data_subscriptions.api.resources.subscribed_dataset.NotSubscribableNotifiationDispatcher"
+    )
+
     dataset_id = all_subscription[0]["dataset_id"]
     response = client.delete(f"/api/v1/dataset/{dataset_id}")
     assert response.status_code == 204
