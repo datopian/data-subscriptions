955a708f05c02a6d7a2a8f3e4653e989d4d520da
anuveyatsu <anuar.ustayev@gmail.com>
2021-04-16 17:05:41 +0600

    [debug][xs]: add logging when syncing dbs so we can understand whats happening.

--- data_subscriptions/worker/latest_ckan_activity.py
+++ data_subscriptions/worker/latest_ckan_activity.py
@@ -1,5 +1,6 @@
 import datetime as dt
 import os
+import logging
 
 from ckanapi import RemoteCKAN
 
@@ -18,12 +19,12 @@ class LatestCKANActivity:
     def __call__(self):
         with RemoteCKAN(
             self.url,
-            user_agent=
-            "data-subscription/latest (API call for CKAN recent activity pull)",
+            user_agent="data-subscription/latest (API call for CKAN recent activity pull)",
         ) as api:
 
           self.activity_list = []
           while True:
+              logging.info(f"LatestCKANActivity: fetching activities {self.server_limit} - {self.current_offset}")
               response = api.action.recently_changed_packages_activity_list(
                   limit=self.server_limit, offset=self.current_offset
               )
@@ -32,7 +33,9 @@ class LatestCKANActivity:
                   self.server_limit = len(response)
               self.current_offset += self.server_limit
               if self.has_reached_the_end(response):
+                  logging.info("LatestCKANActivity: has reached the end - stop")
                   break
+          logging.info("LatestCKANActivity: return list of activities")
           return self.activity_list
 
     def filter_response_for_time_range(self, response):
